bdea203f0593868ce52caec66302497ae8800293
totesccp <tomascvils@gmail.com>
2022-07-03 15:32:03 -0300

    Movimento

---
+++ Jogo RPG V1/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    private float timeToMove = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.up));
+
+        if (Input.GetKey(KeyCode.A) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.left));
+
+        if (Input.GetKey(KeyCode.S) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.down));
+
+        if (Input.GetKey(KeyCode.D) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.right));
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while(elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+    
+}
