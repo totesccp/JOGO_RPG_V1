210b18408ffccc4e3774599060d8b86c4ab6f4bf
totesccp <tomascvils@gmail.com>
2022-07-04 00:39:14 -0300

    Colisores outros obstaculos

--- Jogo RPG V1/Assets/Script/PlayerMovement.cs
+++ Jogo RPG V1/Assets/Script/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private bool isMoving;
     private Vector3 origPos, targetPos;
     private float timeToMove = 0.2f;
+   
 
     // Update is called once per frame
     void Update()
